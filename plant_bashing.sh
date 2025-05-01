049961bbfa79f2f0ad02f9e823020b45a33c2f27
Colton <cwardrop27@gmail.com>
2025-05-01 15:26:48 -0500

    Changed play game y/n logic

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-echo "Hey neighbor I'm Sam, Welcome to my lawn. What is your name?"
+echo "Hey neighbor I'm Joe, Welcome to my lawn. What is your name?"
 
 read name
 sleep 1
-echo "Ok $name, do you want to help me grow a plant.(yes/no)"
+echo "Ok $name, do you want to help me grow a plant."
+echo "please type 'yes' or 'no'"
 
 read answer
 if [[ "$answer" == "yes" || "$answer" == "Yes" ]]; then 
@@ -12,7 +13,9 @@ if [[ "$answer" == "yes" || "$answer" == "Yes" ]]; then
 	echo "Alright, just dig a good sized hole with this trowel and plant this seed."
 	sleep 2
 	echo "Good work!"
-else
+elif [[ "$answer" == "no" || "$answer" == "No" ]];then
 	sleep 2
 	echo "Alright, see you later neighbor!"
+else
+	echo "Please try again, and only use 'yes' or 'no'"
 fi

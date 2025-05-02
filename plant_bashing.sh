7edf22b76569f3d15b4423919235646979e6594c
Colton <cwardrop27@gmail.com>
2025-05-02 15:25:50 -0500

    did sapling development text

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -18,7 +18,7 @@ if [[ "$answer" == "yes" || "$answer" == "Yes" ]]; then
 	sleep 3
 	echo "Our days can take as long as your seconds or minutes."
 	sleep 2
-	echo "Do you want to wait 1 day?"
+	echo "Do you want to wait 2 days?"
 	read answer2
 fi
 
@@ -32,11 +32,11 @@ fi
 
 if [[ "$answer2" == "yes" || "$answer2" == "Yes" ]]; then 
 	sleep 3
-	echo "Alright, our seed has germinated overnight."
+	echo "Alright, our seed has germinated overnight!"
 	sleep 2
 	echo "Germination is the 2nd stage of our plant."
 	sleep 2
-	echo "Do you want to wait another day?"
+	echo "Do you want to wait another 3 days?"
 	read answer3
 
 elif [[ "$answer2" == "no" || "$answer2" == "No" ]];then
@@ -47,4 +47,19 @@ else
 	echo "Please try again, and only use 'yes' or 'no'"
 fi
 
+if [[ "$answer3" == "yes" || "$answer3" == "Yes" ]]; then 
+	sleep 4
+	echo "Alright, our seed has developed into a sapling."
+	sleep 2
+	echo "Germination is the 2nd stage of our plant."
+	sleep 2
+	echo "Do you want to wait another day?"
+	read answer3
 
+elif [[ "$answer3" == "no" || "$answer3" == "No" ]];then
+	sleep 2
+	echo "Alright, see you later neighbor!"
+fi
+else
+	echo "Please try again, and only use 'yes' or 'no'"
+fi

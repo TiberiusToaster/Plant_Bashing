7987963084d5df130c1c641b35795b1d0fd62dfb
Colton <cwardrop27@gmail.com>
2025-05-02 15:17:42 -0500

    solved problem with answer 2 variable

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -14,11 +14,13 @@ if [[ "$answer" == "yes" || "$answer" == "Yes" ]]; then
 	sleep 2
 	echo "Good work!"
 	sleep 1
-	echo "This plant has been genetically engineered to grow much faster than other plants."
+	echo "I know you've only just moved here but I'm sure you've realized time moves much faster here."
 	sleep 3
-	echo "This should take around 5 more seconds to grow, do you want to wait?"
+	echo "Our days can take as long as your seconds or minutes."
 	sleep 2
+	echo "Do you want to wait 1 day?"
 	read answer2
+fi
 
 elif [[ "$answer" == "no" || "$answer" == "No" ]];then
 	sleep 2
@@ -28,16 +30,21 @@ else
 	echo "Please try again, and only use 'yes' or 'no'"
 fi
 
-if [[ "$answer" == "yes" || "$answer" == "Yes" ]]; then 
-	sleep 1
-	echo "Okay"
-	sleep 5
-	echo "Alright, our seed has germinated"
-	
-elif [[ "$answer" == "no" || "$answer" == "No" ]];then
+if [[ "$answer2" == "yes" || "$answer2" == "Yes" ]]; then 
+	sleep 3
+	echo "Alright, our seed has germinated overnight."
+	sleep 2
+	echo "Germination is the 2nd stage of our plant."
+	sleep 2
+	echo "Do you want to wait another day?"
+	read answer3
+
+elif [[ "$answer2" == "no" || "$answer2" == "No" ]];then
 	sleep 2
 	echo "Alright, see you later neighbor!"
 fi
 else
 	echo "Please try again, and only use 'yes' or 'no'"
 fi
+
+

07dcb007d3efe449123b65e43424a423da028002
Colton <cwardrop27@gmail.com>
2025-05-05 14:56:15 -0500

    Fixed some grammar and gameplay errors. started the repeating growing section.

--- plant_bashing.sh
+++ plant_bashing.sh
@@ -49,14 +49,65 @@ fi
 
 if [[ "$answer3" == "yes" || "$answer3" == "Yes" ]]; then 
 	sleep 4
-	echo "Alright, our seed has developed into a sapling."
+	echo "Alright, our seed has developed into a sapling!"
 	sleep 2
-	echo "Germination is the 2nd stage of our plant."
-	sleep 2
+	echo "From now on, your plant will grow taller and stronger, but it won't go through any new stages."
+	sleep 4
 	echo "Do you want to wait another day?"
-	read answer3
+	read answer4
 
 elif [[ "$answer3" == "no" || "$answer3" == "No" ]];then
+	sleep 2
+	echo "Alright, see you later neighbor!"
+fi
+else
+	echo "Please try again, and only use 'yes' or 'no'"
+fi
+
+if [[ "$answer4" == "yes" || "$answer4" == "Yes" ]]; then 
+	sleep 2
+	echo "Alright, your plant has grown 2 cm higher and grown 2 more leaves!"
+	sleep 2
+	echo "Your plant is now 2 cm high and has 2 leaves."
+	sleep 2
+	echo "Do you want to wait another day?"
+	read answer5
+
+elif [[ "$answer4" == "no" || "$answer4" == "No" ]];then
+	sleep 2
+	echo "Alright, see you later neighbor!"
+fi
+else
+	echo "Please try again, and only use 'yes' or 'no'"
+fi
+
+if [[ "$answer5" == "yes" || "$answer5" == "Yes" ]]; then 
+	sleep 2
+	echo "Alright, your plant has grown 2 cm higher and grown 2 more leaves!"
+	sleep 2
+	echo "Your plant is now 4 cm high and has 4 leaves."
+	sleep 2
+	echo "Do you want to wait another day?"
+	read answer6
+
+elif [[ "$answer5" == "no" || "$answer5" == "No" ]];then
+	sleep 2
+	echo "Alright, see you later neighbor!"
+fi
+else
+	echo "Please try again, and only use 'yes' or 'no'"
+fi
+
+if [[ "$answer6" == "yes" || "$answer6" == "Yes" ]]; then 
+	sleep 2
+	echo "Alright, your plant has grown 2 cm higher and grown 2 more leaves!"
+	sleep 2
+	echo "Your plant is now 6 cm high and has 6 leaves."
+	sleep 2
+	echo "Do you want to wait another day?"
+	read answer7
+
+elif [[ "$answer6" == "no" || "$answer6" == "No" ]];then
 	sleep 2
 	echo "Alright, see you later neighbor!"
 fi
